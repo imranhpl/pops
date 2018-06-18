7582e26aea290609e168b350434b813378b1de26
Imran <Imran@ImranS>
2018-06-18 19:10:34 +0530

    First test commit

---
+++ popsRegression/src/pageObjects/LoginPage.java
@@ -0,0 +1,25 @@
+package pageObjects;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class LoginPage {
+	private static WebElement element = null;
+	 
+	 public static WebElement signUp(WebDriver driver){
+	 
+	    element = driver.findElement(By.xpath("//a[@href='https://www.popssoftware.com/sign-up'][text()='Sign Up']"));
+	 
+	    return element;//commit
+	 
+	    }
+	 
+	 public static WebElement signIn(WebDriver driver){
+		 
+		    element = driver.findElement(By.xpath("//a[@href='https://www.popssoftware.com/signin'][text()='Sign In']"));
+		 
+		    return element;
+		 
+		    }
+}

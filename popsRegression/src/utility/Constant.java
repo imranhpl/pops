5c5784709f96371f366790e6277782d38c4378bb
Imran <Imran@ImranS>
2018-06-19 13:11:08 +0530

    last commit

---
+++ popsRegression/src/utility/Constant.java
@@ -0,0 +1,9 @@
+package utility;
+
+public class Constant {
+	public static final String URL = "https://www.popssoftware.com/";
+	 
+    public static final String Username = "company_new_1@yopmail.com";
+
+    public static final String Password = "jail1992";
+}

20084900a17ddf719e999bdbbb9f5915aba33f2c
Imran <Imran@ImranS>
2018-06-19 13:25:30 +0530

    new commit

--- popsRegression/src/pageObjects/LoginPage.java
+++ popsRegression/src/pageObjects/LoginPage.java
@@ -15,9 +15,31 @@ public class LoginPage {
 	 
 	    }
 	 
-	 public static WebElement signIn(WebDriver driver){
+	 public static WebElement signInButton(WebDriver driver){
 		 
 		    element = driver.findElement(By.xpath("//a[@href='https://www.popssoftware.com/signin'][text()='Sign In']"));
+		 
+		    return element;
+		 
+		    }
+	 public static WebElement email(WebDriver driver){
+		 
+		    element = driver.findElement(By.xpath("//input[@class='form-control'][@name='email']"));
+		 
+		    return element;
+		 
+		    }
+	 
+	 public static WebElement password(WebDriver driver){
+		 
+		    element = driver.findElement(By.xpath("//input[@class='form-control'][@name='password']"));
+		 
+		    return element;
+		 
+		    }
+	 public static WebElement loginButton(WebDriver driver){
+		 
+		    element = driver.findElement(By.xpath("//button[@type='submit']"));
 		 
 		    return element;
 		 
